a31179411bfeb309e3c7fa293c9e13980d626803
IsaacSernaT <sernatamayoi310108@gmail.com>
2025-04-30 22:01:49 -0500

    Index principal, script inicial y css principal
    Creación de la página

---
+++ script.js
@@ -0,0 +1,10 @@
+// Cambiar el fondo de la barra de navegación al hacer scroll
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
